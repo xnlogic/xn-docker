910a79ed65e12168fd72a3a54e76843f840b0f00
Darrick Wiebe <dw@xnlogic.com>
2015-06-08 23:40:38 -0400

    updates

--- datomic-free/run.sh
+++ datomic-free/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ip=$(hostname -i)
-sed "s/alt-host=127.0.0.1/alt-host=$ip/" -i /etc/datomic/xn-free.properties
+echo Configuring Datomic for IP: $ip
+sed "s/alt-host=.*/alt-host=$ip/" -i /etc/datomic/xn-free.properties
 
 exec /usr/share/datomic/bin/transactor /etc/datomic/xn-free.properties
